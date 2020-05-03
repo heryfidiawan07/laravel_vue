19ffa8c011dcb29dfba94d42d42c1bb0d950740d
heryfidiawan <heryfidiawan07@gmail.com>
2020-05-03 23:01:11 +0700

    questions show

---
+++ resources/views/questions/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h3>{{$question->title}}</h3>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    {!! $question->body_html !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
