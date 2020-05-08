ef16a0154801c2a7e790bcc1bacd8a32e70a1ea8
heryfidiawan <heryfidiawan07@gmail.com>
2020-05-09 00:13:36 +0700

    Update answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -20,14 +20,33 @@
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
-                            <div class="float-right">
-                                <span class="text-muted">Answer {{$answer->created_date}} </span>
-                                <div class="media">
-                                    <a href="{{$answer->user->url}}" class="pr-2">
-                                        <img src="{{$answer->user->avatar}}">
-                                    </a>
-                                    <div class="media-body">
-                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="ml-auto">
+                                        @if (Auth::check())
+                                            @can('update', $answer)
+                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-outline-info btn-sm">edit</a>
+                                            @endcan
+                                            @can('delete', $answer)
+                                                <form method="POST" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" class="form-delete">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-outline-danger btn-sm" onclick="return confirm('Are you sure ?')">Delete</button>
+                                                </form>
+                                            @endcan
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="col-4"></div>
+                                <div class="col-4">
+                                    <span class="text-muted">Answer {{$answer->created_date}} </span>
+                                    <div class="media">
+                                        <a href="{{$answer->user->url}}" class="pr-2">
+                                            <img src="{{$answer->user->avatar}}">
+                                        </a>
+                                        <div class="media-body">
+                                            <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
